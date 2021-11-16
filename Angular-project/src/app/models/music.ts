a58d425409c1988435752b4e958b4c567dff4b9e
Sebastian Nicolas Pavez Altamirano PC Personal <seba.pawsup@hotmail.com>
2021-11-15 23:28:02 -0300

    Angular project

---
+++ Angular-project/src/app/models/music.ts
@@ -0,0 +1,22 @@
+export class Music {
+    // public album: string;
+    // public artist: string;
+    // public year: number;
+    // public genre: string;
+
+    // constructor(album, artist, year, genre) {
+    //     this.album = album;
+    //     this.artist = artist;
+    //     this.year = year;
+    //     this.genre = genre;
+    // }
+    constructor(
+        public album: string,
+        public artist: string,
+        public year: number,
+        public genre: string, 
+        public wingrammy: boolean
+    ){}
+
+
+}
--- Angular-project/src/app/music/music.component.ts
+++ Angular-project/src/app/music/music.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { Music } from '../models/music';
 
 @Component({
     selector: 'music',
@@ -6,28 +8,55 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 })
 
 //Hooks: OnInit, DoCheck, OnDestroy los principales
-export class MusicComponent implements OnInit, DoCheck, OnDestroy{ 
+export class MusicComponent implements OnInit { 
     public titulo: string;
     public listado: string;
+    public musica: Array<Music>;
+    public artistas: String[];
+    public color: string;
+
     constructor(){
+        this.artistas = new Array();
+        this.color = "red";
         this.titulo = "Componente de Musica";
         this.listado = "Listado de la música más popular";
         console.log("Se ha cargado el componente: videojuego.component.ts");
+
+        this.musica = [
+            new Music('Kick II', 'Arca', 2021, 'Experimental', false),
+            new Music('Kick I', 'Arca', 2020, 'Experimental', true),
+            new Music('Kick III', 'Arca', 2021, 'Experimental', false),
+            new Music('Felt Mountain', 'Goldfrapp', 2000, 'Synthpop', true),
+        ];
     }
 
     ngOnInit(){
-        console.log("OnInit ejecutado");
+        console.log(this.musica);
+
+        this.getArtist();
     }
 
-    ngDoCheck(){
-        console.log("DoCheck ejecutado");
-    }
+    getArtist(){
+        this.musica.forEach((value, index) => {
 
-    ngOnDestroy(){
-        console.log("OnDestroy ejecutado");
-    }
+            if(this.artistas.indexOf(value.artist) < 0){
+                this.artistas.push(value.artist);
+            }
 
-    cambiarTitulo(){
-        this.titulo = "Nuevo Titulo";
+            console.log(this.artistas);
+        });
     }
+    // ngDoCheck(){
+    //     console.log("DoCheck ejecutado");
+    // }
+
+    // ngOnDestroy(){
+    //     console.log("OnDestroy ejecutado");
+    // }
+
+    // cambiarTitulo(){
+    //     this.titulo = "Nuevo Titulo";
+    // }
+
+
 }
